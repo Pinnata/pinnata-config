086010dba8602cf000b390e3d351829c71fd0aaa
Kyle Scott <kyscott@umich.edu>
2022-04-17 11:09:37 -0400

    yarn fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  extends: ["@saberhq"],
+  extends: ["@saberhq/eslint-config"],
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: "tsconfig.json",
   },
 };
