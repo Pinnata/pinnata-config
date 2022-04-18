5710d29e44bdf0c2f954d078ae6e6777aa2b12ea
Kyle Scott <kyscott@umich.edu>
2022-04-18 11:03:14 -0400

    external rewarders

--- src/farms.ts
+++ src/farms.ts
@@ -8,7 +8,7 @@ export enum Protocol {
   Ubeswap,
 }
 
-export interface IProtocol {
+export interface DisplayProtocol {
   protocol: Protocol;
   name: string;
   color: string;
@@ -16,7 +16,7 @@ export interface IProtocol {
   url: string;
 }
 
-export const Ubeswap: IProtocol = {
+export const Ubeswap: DisplayProtocol = {
   protocol: Protocol.Ubeswap,
   name: "Ubeswap",
   color: "#8878c3",
@@ -31,12 +31,12 @@ const sushiProtocolInfo = {
   url: "https://sushi.com",
 };
 
-export const SushiV1: IProtocol = {
+export const SushiV1: DisplayProtocol = {
   ...sushiProtocolInfo,
   name: "Sushiswap v1",
 };
 
-export const SushiV2: IProtocol = {
+export const SushiV2: DisplayProtocol = {
   ...sushiProtocolInfo,
   name: "Sushiswap v2",
 };
@@ -45,10 +45,12 @@ export type IFarm = {
   wrapper: string;
   spell: string;
   id?: string;
+  rewarder: string;
+  externalRewarder?: string;
   lp: string;
   underlying: readonly [Token, Token];
   rewards: readonly Token[];
-  protocol: IProtocol;
+  protocol: DisplayProtocol;
 };
 
 export const Farms: { [chainId in ChainId]: readonly IFarm[] } = {
@@ -57,6 +59,10 @@ export const Farms: { [chainId in ChainId]: readonly IFarm[] } = {
       wrapper: getAddress("0xE583FeC0B218bB89CbB24d76D2A6D901E082DAAA"),
       spell: getAddress("0x4163A7dB783D3d6d761Bd9060EcDe42D1C2D8c74"),
       lp: getAddress("0x0b655E7D966CB27998af94AA5719ab7BFe07D3b3"),
+      rewarder: getAddress("0xE583FeC0B218bB89CbB24d76D2A6D901E082DAAA"),
+      externalRewarder: getAddress(
+        "0x1be211d8da40bc0ae8719c6663307bfc987b1d6c"
+      ),
       id: "3",
       protocol: SushiV1,
       underlying: [CEUR[ChainId.Mainnet], CUSD[ChainId.Mainnet]],
@@ -66,7 +72,11 @@ export const Farms: { [chainId in ChainId]: readonly IFarm[] } = {
       wrapper: getAddress("0x1C4da0695aE25847260628d7eA92c5d336Fe1998"),
       spell: getAddress("0x70CFe574715213782B3BcCfFcbb8d4a298388de7"),
       lp: getAddress("0x0b655E7D966CB27998af94AA5719ab7BFe07D3b3"),
+      rewarder: getAddress("0x8084936982D089130e001b470eDf58faCA445008"),
       id: "3",
+      externalRewarder: getAddress(
+        "0xfa3de59edd2500ba725dad355b98e6a4346ada7d"
+      ),
       protocol: SushiV2,
       underlying: [CUSD[ChainId.Mainnet], CEUR[ChainId.Mainnet]],
       rewards: [SUSHI[ChainId.Mainnet], CELO[ChainId.Mainnet]],
@@ -75,7 +85,11 @@ export const Farms: { [chainId in ChainId]: readonly IFarm[] } = {
       wrapper: getAddress("0x1C4da0695aE25847260628d7eA92c5d336Fe1998"),
       spell: getAddress("0x70CFe574715213782B3BcCfFcbb8d4a298388de7"),
       lp: getAddress("0x8ecded81a2abf3b7e724978060739edbeb01b24f"),
+      rewarder: getAddress("0x8084936982D089130e001b470eDf58faCA445008"),
       id: "8",
+      externalRewarder: getAddress(
+        "0xfa3de59edd2500ba725dad355b98e6a4346ada7d"
+      ),
       protocol: SushiV2,
       underlying: [CELO[ChainId.Mainnet], MOBI[ChainId.Mainnet]],
       rewards: [SUSHI[ChainId.Mainnet], CELO[ChainId.Mainnet]],
@@ -84,6 +98,7 @@ export const Farms: { [chainId in ChainId]: readonly IFarm[] } = {
       wrapper: getAddress("0xFab4224Ce8E71e2f8F95f63a088d828d5B570e12"),
       spell: getAddress("0x7B775b2AF169D1249db545Cd89754D3C70FAd069"),
       lp: getAddress("0x0b81cf47c8f97275d14c006e537d5101b6c87300"),
+      rewarder: getAddress("0xb450940c5297e9b5e7167FAC5903fD1e90b439b8"),
       protocol: Ubeswap,
       underlying: [CELO[ChainId.Mainnet], MOBI[ChainId.Mainnet]],
       rewards: [
@@ -96,6 +111,7 @@ export const Farms: { [chainId in ChainId]: readonly IFarm[] } = {
       wrapper: getAddress("0x1B9dF6fd569778f48E7db3eB000C93a80920EA23"),
       spell: getAddress("0x7B775b2AF169D1249db545Cd89754D3C70FAd069"),
       lp: getAddress("0xe7b5ad135fa22678f426a381c7748f6a5f2c9e6c"),
+      rewarder: getAddress("0x9D87c01672A7D02b2Dc0D0eB7A145C7e13793c3B"),
       protocol: Ubeswap,
       underlying: [CELO[ChainId.Mainnet], UBE[ChainId.Mainnet]],
       rewards: [UBE[ChainId.Mainnet], CELO[ChainId.Mainnet]],
